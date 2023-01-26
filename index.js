13d9de75892138befb91b7a690d5d36509e0213d
Jessyjdi <116257968+Jessyjdi@users.noreply.github.com>
2023-01-25 23:11:36 -0800

    Added Style sheet and updated the code

--- index.js
+++ index.js
@@ -189,11 +189,11 @@ const deleteHtmlFile = () =>{
 };
 
 const mainHtmlFile = () =>{
-    fs.appendFileSync("index.html", generateHtml());
+    fs.appendFileSync("./dist/index.html", generateHtml());
 };
 
 const endOfHtmlFile = () =>{
-    fs.appendFileSync('index.html', endHtml());
+    fs.appendFileSync('./dist/index.html', endHtml());
 }
 
 const generateHtml = () =>{
@@ -203,6 +203,7 @@ const generateHtml = () =>{
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Team Profile Generator</title>
+        <link rel="stylesheet" type="text/css" href="./style.css">
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
@@ -221,7 +222,7 @@ const generateHtml = () =>{
 
 const generateManager = () =>{
     managers.forEach((Manager => {
-        fs.appendFileSync('index.html', generateMgr(Manager))
+        fs.appendFileSync('./dist/index.html', generateMgr(Manager))
     }))
 
 }
@@ -230,7 +231,8 @@ const generateMgr = (Manager) =>{
     return `<div class="row row-cols-1 row-cols-md-3 g-4">
     <div class="col">
       <div class="card text-bg-primary" style="width: 18rem">
-        <h2 class="text-center manager">Manger
+        <h2> ${Manager.name}</h2>
+        <h3 class="text-left manager">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cup-hot-fill" viewBox="0 0 16 16" >
             <path
               fill-rule="evenodd"
@@ -239,8 +241,8 @@ const generateMgr = (Manager) =>{
             <path
               d="m4.4.8-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.31 3.31 0 0 1-.202.388 5.444 5.444 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 3.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 3.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 3 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 4.4.8Zm3 0-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.31 3.31 0 0 1-.202.388 5.444 5.444 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 6.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 6.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 6 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 7.4.8Zm3 0-.003.004-.014.019a4.077 4.077 0 0 0-.204.31 2.337 2.337 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.198 3.198 0 0 1-.202.388 5.385 5.385 0 0 1-.252.382l-.019.025-.005.008-.002.002A.5.5 0 0 1 9.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 9.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 9 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 10.4.8Z"
             />
-          </svg>
-        </h2>
+          </svg>  Manger
+        </h3>
         <div class="card-body"></div>
         <ul class="list-group list-group-flush">
           <li class="list-group-item">Name: ${Manager.name}</li>
@@ -257,18 +259,19 @@ const generateMgr = (Manager) =>{
 
 const generateEngineer = () => {
     engineers.forEach((engineer => {
-        fs.appendFileSync('index.html', generateEng(engineer));
+        fs.appendFileSync('./dist/index.html', generateEng(engineer));
     }));
 };
 
 const generateEng =(Engineer) =>{
     return `<div class="col">
     <div class="card text-bg-primary" style="width: 18rem">
-      <h2 class="text-center engineer">Engineer
+    <h2> ${Engineer.name}</h2>
+      <h3 class="text-left engineer">
        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eyeglasses" viewBox="0 0 16 16">
 <path d="M4 6a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm2.625.547a3 3 0 0 0-5.584.953H.5a.5.5 0 0 0 0 1h.541A3 3 0 0 0 7 8a1 1 0 0 1 2 0 3 3 0 0 0 5.959.5h.541a.5.5 0 0 0 0-1h-.541a3 3 0 0 0-5.584-.953A1.993 1.993 0 0 0 8 6c-.532 0-1.016.208-1.375.547zM14 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
-</svg>
-      </h2>
+</svg> Engineer
+      </h3>
       <div class="card-body"></div>
       <ul class="list-group list-group-flush">
         <li class="list-group-item">Name: ${Engineer.name}</li>
@@ -290,19 +293,20 @@ const generateEng =(Engineer) =>{
 
 const generateIntern = () => {
     interns.forEach((intern => {
-        fs.appendFileSync('index.html', generateInt(intern));
+        fs.appendFileSync('./dist/index.html', generateInt(intern));
     }));
 }
 
 const generateInt =(Intern) =>{
     return `<div class="col">
     <div class="card text-bg-primary" style="width: 18rem">
-      <h2 class="text-center intern">Intern
+    <h2> ${Intern.name}</h2>
+      <h3 class="text-left intern">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mortarboard-fill" viewBox="0 0 16 16">
             <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5Z"/>
             <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Z"/>
-          </svg>
-      </h2>
+          </svg> Intern
+      </h3>
       <div class="card-body"></div>
       <ul class="list-group list-group-flush">
         <li class="list-group-item">Name: ${Intern.name}</li>
